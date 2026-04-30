5e425959d4b5fc45258751b54f135faab7abcb4e
Nurbu <lamanurbu2@gmail.com>
2026-04-29 21:18:16 -0400

    set my icon to stage and added comments to help me remember hierarchy in html terms.

--- src/main/java/com/pluralsight/HelloFX.java
+++ src/main/java/com/pluralsight/HelloFX.java
@@ -3,6 +3,7 @@ package com.pluralsight;
 import javafx.application.Application;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
@@ -15,9 +16,14 @@ public class HelloFX extends Application {
 
     @Override
     public void start(Stage stage) throws Exception {
+        // Similar to div can only have 1 in java.
         Group root = new Group();
-        Scene scene = new Scene(root, Color.BLACK);
 
+        // similar to HTML doc
+        Scene scene = new Scene(root, Color.BLACK);
+        Image logo = new Image(getClass().getClassLoader().getResourceAsStream("ledger-icon.webp"));
+        stage.getIcons().add(logo);
+        // Window
         stage.setTitle("First JavaFX project");
         stage.setScene(scene);
         stage.show();
