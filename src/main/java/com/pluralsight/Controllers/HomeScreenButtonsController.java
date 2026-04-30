ceefbf45f05db6c778fbb47cd45aec140617477a
Nurbu <lamanurbu2@gmail.com>
2026-04-30 11:57:56 -0400

    Added routing to buttons for Home Screen, created layout of LedgerMenu and created initial LedgerMenuButtonsController and ReportsMenuButtonsController files.

--- src/main/java/com/pluralsight/Controllers/HomeScreenButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/HomeScreenButtonsController.java
@@ -2,11 +2,17 @@ package com.pluralsight.Controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Button;
 import javafx.scene.control.ButtonType;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class HomeScreenButtonsController {
     @FXML
@@ -14,7 +20,33 @@ public class HomeScreenButtonsController {
     @FXML
     private AnchorPane scenePane;
 
-    Stage stage;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
+
+    public void switchToAddDeposit(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AddDeposit.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToAddPayment(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("AddPayment.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void switchToLedgerMenu(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("LedgerMenu.fxml"));
+        stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 
     public void exit(ActionEvent event) {
 
