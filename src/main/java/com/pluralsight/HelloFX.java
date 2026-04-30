da98586fe93865d6b84855089364f10f21c4d691
Nurbu <lamanurbu2@gmail.com>
2026-04-29 21:52:23 -0400

    Experimented with putting text in root and adding that to scene.

--- src/main/java/com/pluralsight/HelloFX.java
+++ src/main/java/com/pluralsight/HelloFX.java
@@ -5,6 +5,9 @@ import javafx.scene.Group;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
@@ -20,9 +23,17 @@ public class HelloFX extends Application {
         Group root = new Group();
 
         // similar to HTML doc
-        Scene scene = new Scene(root, Color.BLACK);
+        Scene scene = new Scene(root, 100, 100, Color.WHITE);
+        stage.setWidth(1000);
+        stage.setHeight(1000);
         Image logo = new Image(getClass().getClassLoader().getResourceAsStream("ledger-icon.webp"));
         stage.getIcons().add(logo);
+        Text text = new Text();
+        text.setText("Welcome to Ledger Account");
+        text.setX(50);
+        text.setY(50);
+        root.getChildren().add(text);
+        text.setFont(Font.font("Verdana", FontWeight.BOLD, 50));
         // Window
         stage.setTitle("First JavaFX project");
         stage.setScene(scene);
