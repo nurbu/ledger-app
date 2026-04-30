81b8d034bffd7e22fd2fb41092cd8ae302a5c1bc
Nurbu <lamanurbu2@gmail.com>
2026-04-29 20:45:35 -0400

    Created first stage, group, and scene, .setTitle("First JavaFX project"). scene background set to black.

--- src/main/java/com/pluralsight/HelloFX.java
+++ src/main/java/com/pluralsight/HelloFX.java
@@ -1,25 +1,25 @@
 package com.pluralsight;
 
 import javafx.application.Application;
+import javafx.scene.Group;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class HelloFX extends Application {
 
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
+    public void start(Stage stage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, Color.BLACK);
+
+        stage.setTitle("First JavaFX project");
         stage.setScene(scene);
         stage.show();
     }
-
-    public static void main(String[] args) {
-        launch();
-    }
-
 }
