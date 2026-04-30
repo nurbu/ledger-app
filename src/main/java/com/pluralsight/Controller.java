087349227becf15744f69d1f07cec3d0fd4a473b
Nurbu <lamanurbu2@gmail.com>
2026-04-30 00:04:49 -0400

    Created a controller class to give buttons actions and change fxml to set version from 26 aka the lastest to version 21 of javafx.

---
+++ src/main/java/com/pluralsight/Controller.java
@@ -0,0 +1,21 @@
+package com.pluralsight;
+
+import javafx.event.ActionEvent;
+
+public class Controller {
+    public void up(ActionEvent e) {
+        System.out.println("UP");
+    }
+
+    public void down(ActionEvent e) {
+        System.out.println("DOWN");
+    }
+
+    public void right(ActionEvent e) {
+        System.out.println("RIGHT");
+    }
+
+    public void left(ActionEvent e) {
+        System.out.println("LEFT");
+    }
+}
