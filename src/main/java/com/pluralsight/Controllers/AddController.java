d3619d448d24fcbec3a96c124f19beb5885bc9da
Nurbu <lamanurbu2@gmail.com>
2026-04-30 14:00:18 -0400

    Created AddDeposit.fxml, AddPayment.fxml, and AddController. Now once options selected they go to their pages. Still have to add functionality to submitButton to handle input.

---
+++ src/main/java/com/pluralsight/Controllers/AddController.java
@@ -0,0 +1,9 @@
+package com.pluralsight.Controllers;
+
+import javafx.event.ActionEvent;
+
+public class AddController {
+
+    public void submitButton(ActionEvent actionEvent) {
+    }
+}
--- src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
@@ -1,5 +1,4 @@
 package com.pluralsight.Controllers;
 
 public class LedgerMenuButtonsController {
-
 }
