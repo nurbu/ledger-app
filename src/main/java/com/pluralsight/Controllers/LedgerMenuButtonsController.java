8d07f8b820ba57fc4288c1648caefcbf6e11fd7c
Nurbu <lamanurbu2@gmail.com>
2026-05-01 01:46:27 -0400

    set the methods for LedgerMenu and added functionality to enter reports menu and return home buttons.

--- src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
@@ -7,6 +7,22 @@ import javafx.event.ActionEvent;
 import java.io.IOException;
 
 public class LedgerMenuButtonsController {
+    public void displayAllTransactions(ActionEvent actionEvent) {
+    }
+
+    public void displayAllDeposits(ActionEvent actionEvent) {
+    }
+
+    public void displayAllPayments(ActionEvent actionEvent) {
+    }
+
+    public void switchToReportsMenu(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.SceneSwitcher(actionEvent, "/ReportsMenu.fxml");
+        } catch (IOException e) {
+            IOExceptionHandler.handleIOException(e);
+        }
+    }
 
     public void returnHome(ActionEvent actionEvent) {
         try {
