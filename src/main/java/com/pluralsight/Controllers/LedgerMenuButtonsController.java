423fed8c032c09314a7c3ebc448bd277b08413aa
Nurbu <lamanurbu2@gmail.com>
2026-05-01 01:34:43 -0400

    Added return home functionality and return to ledger Menu functionality.

--- src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/LedgerMenuButtonsController.java
@@ -1,4 +1,18 @@
 package com.pluralsight.Controllers;
 
+import com.pluralsight.IOExceptionHandler;
+import com.pluralsight.util.SceneSwitcher;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class LedgerMenuButtonsController {
+
+    public void returnHome(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.SceneSwitcher(actionEvent, "/HomeScreen.fxml");
+        } catch (IOException e) {
+            IOExceptionHandler.handleIOException(e);
+        }
+    }
 }
--- src/main/java/com/pluralsight/Controllers/ReportsMenuButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/ReportsMenuButtonsController.java
@@ -1,4 +1,17 @@
 package com.pluralsight.Controllers;
 
+import com.pluralsight.IOExceptionHandler;
+import com.pluralsight.util.SceneSwitcher;
+import javafx.event.ActionEvent;
+
+import java.io.IOException;
+
 public class ReportsMenuButtonsController {
+    public void returnLedgerMenu(ActionEvent actionEvent) {
+        try {
+            SceneSwitcher.SceneSwitcher(actionEvent, "/LedgerMenu.fxml");
+        } catch (IOException e) {
+            IOExceptionHandler.handleIOException(e);
+        }
+    }
 }
