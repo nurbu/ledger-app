4e496b4a844861fd192d239ba41400d46b2bb705
Nurbu <lamanurbu2@gmail.com>
2026-05-01 03:45:41 -0400

    Fixed bug in ReportsMenuButtonsController put new TableView() instead of using the prebuilt TableHeaderPrep.

--- src/main/java/com/pluralsight/Controllers/ReportsMenuButtonsController.java
+++ src/main/java/com/pluralsight/Controllers/ReportsMenuButtonsController.java
@@ -1,11 +1,13 @@
 package com.pluralsight.Controllers;
 
 import com.pluralsight.IOExceptionHandler;
+import com.pluralsight.TableHeaderPrep;
 import com.pluralsight.Transaction;
 import com.pluralsight.TransactionData;
 import com.pluralsight.util.SceneSwitcher;
 import javafx.event.ActionEvent;
 import javafx.scene.Group;
+import javafx.scene.Scene;
 import javafx.scene.control.TableView;
 import javafx.stage.Stage;
 
@@ -19,25 +21,60 @@ public class ReportsMenuButtonsController {
         LocalDate start = today.withDayOfMonth(1);
         Stage stage = new Stage();
         Group root = new Group();
-        TableView<Transaction> table = new TableView<>();
+        TableView<Transaction> table = TableHeaderPrep.tableBuilder();
         root.getChildren().add(table);
         table.setItems(TransactionData.filterTransactionsByDate(start, today));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void previousMonth(ActionEvent actionEvent) {
+        LocalDate firstDayOfTheMonth = LocalDate.now().withDayOfMonth(1);
+        LocalDate start = firstDayOfTheMonth.minusMonths(1);
+        LocalDate end = firstDayOfTheMonth.minusDays(1);
+        Stage stage = new Stage();
+        Group root = new Group();
+        TableView<Transaction> table = TableHeaderPrep.tableBuilder();
+        root.getChildren().add(table);
+        table.setItems(TransactionData.filterTransactionsByDate(start, end));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void yearToDate(ActionEvent actionEvent) {
+        LocalDate today = LocalDate.now();
+        LocalDate start = today.withDayOfYear(1);
+        Stage stage = new Stage();
+        Group root = new Group();
+        TableView<Transaction> table = TableHeaderPrep.tableBuilder();
+        root.getChildren().add(table);
+        table.setItems(TransactionData.filterTransactionsByDate(start, today));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void previousYear(ActionEvent actionEvent) {
+        int lastYear = LocalDate.now().getYear() - 1;
+        LocalDate start = LocalDate.of(lastYear, 1, 1);
+        LocalDate end = LocalDate.of(lastYear, 12, 31);
+        Stage stage = new Stage();
+        Group root = new Group();
+        TableView<Transaction> table = TableHeaderPrep.tableBuilder();
+        root.getChildren().add(table);
+        table.setItems(TransactionData.filterTransactionsByDate(start, end));
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public void searchByVendor(ActionEvent actionEvent) {
 
     }
 
-    public void monthToDate(ActionEvent actionEvent) {
-
-    }
-
-    public void monthToDate(ActionEvent actionEvent) {
-
-    }
-
-    public void monthToDate(ActionEvent actionEvent) {
-
-    }
-
-    public void monthToDate(ActionEvent actionEvent) {
+    public void customSearch(ActionEvent actionEvent) {
     }
 
     public void returnLedgerMenu(ActionEvent actionEvent) {
